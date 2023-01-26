e9207839e9f6dc875926689b9d785a201d583cc6
Collins Tatang <makungongcollins@gmail.com>
2023-01-26 11:50:23 +0100

    Update main style

--- src/component/Main.js
+++ src/component/Main.js
@@ -6,13 +6,28 @@ function Main() {
 return(
     <main>
         <img src={main} alt="logo" className="main-photo" />
-        <div className='submain'>
-             <h3>Laura Smith</h3>
-             <h5>Frontend Developer</h5>
-             <h6>laurasmith.website</h6>
-            <button className='email-btn'>Email</button>
-            <button className='linkedin-btn'>Email</button>
-        </div>
+        <div className='mainContent'>
+            <div className='submain'>
+                <h3>Laura Smith</h3>
+                <h5>Frontend Developer</h5>
+                <h6>laurasmith.website</h6>
+                <button className='email-btn'>Email</button>
+                <button className='linkedin-btn'>Email</button>
+            </div>
+            <div className="content">
+                <h3>About</h3>
+                <p>I am a frontend developer with a particular
+                interest in making things simple and automating
+                daily tasks. I try to keep up with security and 
+                best practices, and am always looking for new things to learn.
+                </p>
+                <h3>Interests</h3>
+                <p>Food expert. Music scholar. Reader. 
+                    Internet fanatic. Bacon buff. Entrepreneur. 
+                    Travel geek. Pop culture ninja. Coffee fanatic.
+                </p>
+            </div>
+        </div>,
         <Footer />
     </main>
 )
