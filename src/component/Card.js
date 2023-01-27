af468ac70afb4f3057b342eadcd8fd83b0d5ab14
Collins Tatang <makungongcollins@gmail.com>
2023-01-27 18:55:56 +0100

    Add distructure to card props

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,16 +1,16 @@
 import "./css/Card.css"
 
-const Card = (props) => {
+const Card = ({img, name, phone, email}) => {
 
     return (
         <div className="card">
-            <img src={props.img} alt="Not found" className="card--pic" />
-            <h3>{props.name}</h3>
+            <img src={img} alt="Not found" className="card--pic" />
+            <h3>{name}</h3>
             <div className="card--stats">
                 <img src={require('../images/start.png')} alt="Not found" className="card--star"/>
-                <span>{props.phone}</span>
+                <span>{phone}</span>
                 <span className="gray">(6) * </span>
-                <span className="gray">{props.email}</span>
+                <span className="gray">{email}</span>
             </div>
             <p>Life lessons of Kadic Magarnal</p>
             <p><span className="bold">From $136</span> / person</p>
